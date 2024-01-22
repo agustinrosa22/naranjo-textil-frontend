d74223e58594f8621f0c55ede9ed9d8238596372
agustinrosa22 <agustinrosa1234@gmail.com>
2024-01-22 12:38:09 -0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './views/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 import { Routes,Route, useLocation } from 'react-router-dom';  
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes> 
        <Route path='/' element={ <Home />} />
       </Routes>
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './NavBar.module.css'
+
+const NavBar = ()=> {
+    return(
+        <div className={styles.navContainer}>
+        <nav className={styles.navbar}>
+            <h2>NAVBAR</h2>
+        </nav>
+        </div>
+    )
+}
+
+export default NavBar
