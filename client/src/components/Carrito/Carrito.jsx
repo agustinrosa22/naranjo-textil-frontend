2f7aa697c778186c703265a19316356260ef6099
agustinrosa22 <agustinrosa1234@gmail.com>
2024-03-21 20:03:02 -0300

    Finalizar Todas las Compras

--- client/src/components/Carrito/Carrito.jsx
+++ client/src/components/Carrito/Carrito.jsx
@@ -23,6 +23,36 @@ const CartView = () => {
     setCart(updatedCart);
   };
 
+  const handleQuantityChange = (productId, newQuantity) => {
+    const updatedCart = cart.map(item => {
+      if (item.id === productId) {
+        return { ...item, cantidad: newQuantity };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const handleSellerChange = (productId, newSeller) => {
+    const updatedCart = cart.map(item => {
+      if (item.id === productId) {
+        return { ...item, vendedor: newSeller };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const handleCommentChange = (productId, newComment) => {
+    const updatedCart = cart.map(item => {
+      if (item.id === productId) {
+        return { ...item, comentario: newComment };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
   const handlePurchase = (productId) => {
     const product = cart.find(item => item.id === productId);
     if (!product) {
@@ -33,13 +63,19 @@ const CartView = () => {
     const saleData = {
       productId: product.id,
       userId: 1, // Aquí debes obtener el ID del usuario de alguna manera
-      cantidad: 1, // Por ahora, supongamos que siempre se compra solo una unidad de cada producto
+      cantidad: product.cantidad,
       costo: product.editedCost || product.costo,
       vendedor: product.vendedor || '',
       comentario: product.comentario || ''
     };
 
     dispatch(sellProduct(saleData));
+  };
+
+  const handleFinalizeAllPurchases = () => {
+    cart.forEach(product => handlePurchase(product.id));
+    setCart([]); // Vaciar el carrito después de finalizar todas las compras
+    localStorage.removeItem('cart');
   };
 
   return (
@@ -54,20 +90,42 @@ const CartView = () => {
               <img src={product.image} alt="" />
               <p>{product.nombreProducto} - ${product.editedCost || product.costo}</p>
               
-              
               <input
                 type="number"
                 placeholder="Descuento (%)"
                 onChange={(e) => handleCostChange(product.id, product.costo * (1 - parseFloat(e.target.value) / 100))}
               />
 
+              <input
+                type="number"
+                placeholder="Cantidad"
+                value={product.cantidad}
+                onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
+              />
+
+              <input
+                type="text"
+                placeholder="Vendedor"
+                value={product.vendedor || ''}
+                onChange={(e) => handleSellerChange(product.id, e.target.value)}
+              />
+
+              <input
+                type="text"
+                placeholder="Comentario"
+                value={product.comentario || ''}
+                onChange={(e) => handleCommentChange(product.id, e.target.value)}
+              />
+
               <button onClick={() => removeFromCart(product.id)}>Eliminar</button>
-              <button onClick={() => handlePurchase(product.id)}>Finalizar Compra</button>
+              {/* <button onClick={() => handlePurchase(product.id)}>Finalizar Compra</button> */}
             </div>
           ))}
         </div>
       )}
+       <button onClick={handleFinalizeAllPurchases}>Finalizar Todas las Compras</button>
     </div>
+    
   );
 };
 
